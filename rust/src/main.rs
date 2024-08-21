c870b1e5560676aedfec5fcc871c98cb620c2509
baiango <105705580+baiango@users.noreply.github.com>
2024-08-21 16:28:21 +0800

    Discarded Rust codes

--- rust/src/main.rs
+++
@@ -1,8 +0,0 @@
-use crystalline_lossy::ycocg;
-
-
-fn main() {
-	println!("Hello, world!");
-	let (y, co, cg) = ycocg::rgb_to_ycocg(255, 255, 255);
-	println!("Y: {}, Co: {}, Cg: {}", y, co, cg);
-}
